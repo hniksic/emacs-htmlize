2e7cb2c4335f6a69773fd0b4f230d263721a2a3a
Hrvoje Niksic <hniksic@gmail.com>
2011-02-19 12:16:04 +0100

    Version 1.37.

--- htmlize.el
+++ htmlize.el
@@ -4,6 +4,7 @@
 
 ;; Author: Hrvoje Niksic <hniksic@xemacs.org>
 ;; Keywords: hypermedia, extensions
+;; Version: 1.37
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -96,7 +97,7 @@
     ;; `cl' is loaded.
     (load "cl-extra")))
 
-(defconst htmlize-version "1.36")
+(defconst htmlize-version "1.37")
 
 ;; Incantations to make custom stuff work without customize, e.g. on
 ;; XEmacs 19.14 or GNU Emacs 19.34.

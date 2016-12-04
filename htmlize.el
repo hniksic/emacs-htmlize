6f9bb1fcf0cab5f2fd9ccee3bbf0de0671571f27
Hrvoje Niksic <hniksic@gmail.com>
2016-12-04 01:06:10 +0100

    Bump version.

--- htmlize.el
+++ htmlize.el
@@ -4,7 +4,7 @@
 
 ;; Author: Hrvoje Niksic <hniksic@xemacs.org>
 ;; Keywords: hypermedia, extensions
-;; Version: 1.48
+;; Version: 1.49
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by

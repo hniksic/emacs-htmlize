1bc2f1b0feb852fa5a289a1d72646b16ac84adf1
Hrvoje Niksic <hniksic@gmail.com>
2018-03-28 23:28:07 +0200

    Bump version.

--- htmlize.el
+++ htmlize.el
@@ -4,7 +4,7 @@
 
 ;; Author: Hrvoje Niksic <hniksic@gmail.com>
 ;; Keywords: hypermedia, extensions
-;; Version: 1.51
+;; Version: 1.53
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -87,7 +87,7 @@
   (defvar font-lock-support-mode)
   (defvar global-font-lock-mode))
 
-(defconst htmlize-version "1.51")
+(defconst htmlize-version "1.53")
 
 (defgroup htmlize nil
   "Convert buffer text and faces to HTML."
